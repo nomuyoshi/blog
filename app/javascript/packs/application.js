fec141e6b13b81f1bb10eaa5fcb23c684d27bef4
nomuyoshi <nomuyoshi0111@gmail.com>
2022-07-14 09:31:06 +0900

    ざっくり全体レイアウト構築

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,3 +11,26 @@ import "channels"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+document.addEventListener('turbolinks:load', () => {
+  console.log('called');
+  // Get all "navbar-burger" elements
+  const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+  // Add a click event on each of them
+  $navbarBurgers.forEach( el => {
+    el.addEventListener('click', () => {
+
+      console.log('click');
+      // Get the target from the "data-target" attribute
+      const target = el.dataset.target;
+      const $target = document.getElementById(target);
+
+      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+      el.classList.toggle('is-active');
+      $target.classList.toggle('is-active');
+
+    });
+  });
+
+});
