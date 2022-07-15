6f9e22e9018ad44adbb0a0d8bb97ad0c03fc6219
nomuyoshi <nomuyoshi0111@gmail.com>
2022-07-15 09:58:31 +0900

    db:seed でカテゴリデータ投入

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,11 @@
 require "csv"
 
-Dir.glob(Rails.root.join("db/seeds/*.csv",)).each do |path|
+# 実行方法: bin/rails db:seed TABLES=t1,t2,t3
+# TABLESで指定したテーブルのみupsert
+# 未指定の場合はdb/seeds/以下のcsvすべてが多少
+pattern = ENV["TABLES"].blank? ? "db/seeds/*.csv" : "db/seeds/{#{ENV["TABLES"]}}.csv"
+
+Dir.glob(Rails.root.join(pattern)).each do |path|
   filename = File.basename(path, ".csv")
   klass = Module.const_get(filename.classify)
   data = CSV.read(path, headers: true).map(&:to_h)
