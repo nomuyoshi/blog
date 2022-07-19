82b2e0934b08e517285ee78bfb632967f6a6054b
nomuyoshi <nomuyoshi0111@gmail.com>
2022-07-20 06:39:36 +0900

    カテゴリ記事一覧ページ

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   resources :articles, only: [:index, :show]
+  root to: "articles#index"
+
+  resources :categories, only: [:show]
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
   namespace :admin do
     resources :articles
     resources :media, only: [:index, :new, :create, :destroy]
   end
-  root to: "articles#index"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
