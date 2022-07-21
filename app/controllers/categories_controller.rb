d8091b88a0ea893d7b08cf182593a41fcba2c9cd
nomuyoshi <nomuyoshi0111@gmail.com>
2022-07-21 09:54:46 +0900

    カテゴリページにパンくず表示

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -2,5 +2,6 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find_by(slug: params[:slug])
     @articles = @category.articles
+    @tree = @category.ancestors
   end
 end
